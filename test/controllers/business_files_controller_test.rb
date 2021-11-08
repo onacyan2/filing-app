09bede0077346d5decbc2a2c60750916d35cdbc4
onacyan2 <omiz733NL@icloud.com>
2021-11-08 12:32:21 +0900

    ユーザー登録画面の修正２

--- test/controllers/business_files_controller_test.rb
+++ test/controllers/business_files_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class FilesControllerTest < ActionDispatch::IntegrationTest
+class BusinessFilesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
