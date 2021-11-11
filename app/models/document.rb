57e601d2285ed6da581f7f1d4651dfd5d03f2230
onacyan2 <omiz733NL@icloud.com>
2021-11-11 09:46:29 +0900

    書類登録機能コントローラーの作成

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,2 +1,7 @@
 class Document < ApplicationRecord
+  belongs_to :business_file
+
+  validates :company_name, presence: true
+  validates :company_name_kana, presence: true
+  validates :title, presence: true
 end
